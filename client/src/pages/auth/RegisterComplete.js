0c1f6926ed2a1b5eecceef7496cac985891e54e8
John Dang <jollyjohndang@gmail.com>
2021-01-09 16:37:01 -0800

    firebase authentication done for now

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -8,13 +8,46 @@ const RegisterComplete = ({history}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    useState(() => {
+    //useEffect will populate the email in the state in line 8
+    useEffect(() => {
         setEmail(window.localStorage.getItem('emailForRegistration'))
+        // console.log(window.location.href);
+        // console.log(window.localStorage.getItem('emailForRegistration'));
     }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    //to implement passwordless authentication through firebase
+        //validation
+        if(!email || !password) {
+            toast.error('Email and password required');
+            return;
+        }
+
+        if(password.length < 6) {
+            toast.error('Password must be 6 characters long');
+            return;
+        }
+    
+    try {
+        const result = await auth.signInWithEmailLink(email, window.location.href);
+        // console.log('RESULT', result)
+        if(result.user.emailVerified) {
+            //remove user email from local storage
+            window.localStorage.removeItem('emailForRegistration');
+            //get user id token
+            //we dont have to save user in local storage cuz firebase gives track of currently logged in user
+            let user = auth.currentUser;
+            await user.updatePassword(password);
+            const idTokenResult = await user.getIdTokenResult();
+            //redux store
+                console.log('user', user, 'idTokenResult', idTokenResult);
+            //redirect
+            history.push('/')
+        }
+    } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+    }
 };
 
     const completeRegisterForm = () => (
