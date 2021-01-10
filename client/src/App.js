86ac72638eb34e761a963ff45581a1b10b607457
John Dang <jollyjohndang@gmail.com>
2021-01-10 15:26:43 -0800

    user redux and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,  {useEffect} from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 // import {Router} from "@reach/router";
@@ -12,9 +12,33 @@ import Login from './pages/auth/Login';
 import Header from './components/nav/Header'
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+import {auth} from './firebase';
+import {useDispatch} from 'react-redux';
+
 
 
 const App = () => {
+
+const dispatch = useDispatch();
+
+//to check firebase auth state
+useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged(async (user) => {
+    if(user) {
+      const idTokenResult = await user.getIdTokenResult();
+      console.log('user', user);
+      dispatch({
+        type: 'LOGGED_IN_USER',
+        payload: {
+          name: user.email,
+          token: idTokenResult.token,
+        },
+      });
+    }
+  });
+  //cleanup
+  return() => unsubscribe();
+}, []);
     
   return (
    <>
--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -2,18 +2,34 @@ import React, {useState} from 'react';
 
 import {Menu} from 'antd';
 
-import { HomeTwoTone, UserOutlined, UserAddOutlined, SettingOutlined } from '@ant-design/icons';
+import { HomeTwoTone, UserOutlined, UserAddOutlined, SettingOutlined, LogoutOutlined } from '@ant-design/icons';
 
 import { Link } from 'react-router-dom';
+//required to implement logout
+import firebase from 'firebase';
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
-    const [current, setCurrent] = useState('home')
+    const [current, setCurrent] = useState('home');
+
+    let dispatch = useDispatch();
+    let history = useHistory();
 
     const handleClick = (e) => {
         setCurrent(e.key);
     }
+    //to log user out of app and firesbase entirely, user is not in state
+    const logout = () => {
+        firebase.auth().signOut();
+        dispatch({
+            type: "LOGOUT",
+            payload: null,
+        });
+        history.push("/login");
+    };
 
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
@@ -36,7 +52,7 @@ const Header = () => {
           
             <Item key="setting:1">Option 1</Item>
             <Item key="setting:2">Option 2</Item>
-          
+            <Item icon={<LogoutOutlined />} onClick={logout}>Log out</Item>
         </SubMenu>
         
       </Menu>
