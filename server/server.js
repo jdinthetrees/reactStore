654069d7853aea6ddea1c5700f2a59c75acee2f4
John Dang <jollyjohndang@gmail.com>
2021-01-15 20:19:14 -0800

    basic server set up

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
 //app
-const app = express()
+const app = express();
 
 //db
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFineAndModify: true,
+  })
+  .then()
+  .catch((err) => console.log(`DB CONNECTION ERR`));
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+//route
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hey you hit node API hiiiiii",
+  });
+});
+
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
