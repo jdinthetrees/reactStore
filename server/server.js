aa2e5853587d5240a64bebb7edb093af144fbdd0
John Dang <jollyjohndang@gmail.com>
2021-01-15 17:49:28 -0800

    .env added to git ignore

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+//app
+const app = express()
+
+//db
+mongoose.connect(process.env.DATABASE)
