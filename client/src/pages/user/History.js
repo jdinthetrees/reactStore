c3241ba7badcc34e415e06819044d4fa0c787c51
John Dang <jollyjohndang@gmail.com>
2021-01-19 15:30:47 -0800

    sidebar nav

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import UserNav from '../../components/nav/UserNav';
 
 const History = () => (
     <div className="container-fluid">
         <div class="row">
+            <div class="col-md-2">
+                <UserNav />
+            </div>
             <div class="col">user history page</div>
         </div>
     </div>
