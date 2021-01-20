fa7a76b1e0702422b1aa5de0d29abc4ea214935d
John Dang <jollyjohndang@gmail.com>
2021-01-19 18:12:33 -0800

    password update

---
+++ client/src/pages/user/Wishlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserNav from '../../components/nav/UserNav';
+
+const Wishlist = () => (
+    <div className="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <UserNav />
+            </div>
+            <div class="col">user wishlist page</div>
+        </div>
+    </div>
+)
+
+export default Wishlist;
