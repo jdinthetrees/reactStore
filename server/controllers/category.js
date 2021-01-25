a737fba844bf0902048ebc0ab31ef365fe533efa
John Dang <jollyjohndang@gmail.com>
2021-01-24 21:07:51 -0800

    CRUD commands for category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -14,16 +14,37 @@ exports.create = async (req, res) => {
 
 exports.list = async (req, res) => {
     //
+    //sort by latest created category
+    res.json(await Category.find({}).sort({createdAt: -1}).exec());
 };
 
 exports.read = async (req, res) => {
     //
+    let category = await Category.findOne({slug: req.params.slug}).exec();
+    res.json(category);
 };
 
 exports.update = async (req, res) => {
     //
-};
+    const {name} = req.body;
+    try {
+        const updated = await Category.findOneAndUpdate(
+            {slug: req.params.slug}, 
+            {name, slug: slugify(name)}, 
+            {new: true}
+            );
+            res.json(update);
+        } catch(err) {
+            res.status(400).send('Category Update failed');
+        }
+    };
+
 
 exports.remove = async (req, res) => {
-    //
+    try {
+      const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+      res.json(deleted);
+    } catch (err) {
+      res.status(400).send("Create delete failed");
+    }
 };
