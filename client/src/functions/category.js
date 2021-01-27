5b0c9eecd1831b9b7a4d08ea1d50e6d773b7bf53
John Dang <jollyjohndang@gmail.com>
2021-01-26 18:13:03 -0800

    created categories and listed categories

--- client/src/functions/category.js
+++ client/src/functions/category.js
@@ -23,7 +23,7 @@ export const updateCategory = async (slug, category, authtoken) =>
     });
     
     export const createCategory = async (category, authtoken) => 
-    await axios.post(`${process.env.REACT_APP_API}/category`, category {
+    await axios.post(`${process.env.REACT_APP_API}/category`, category, {
         headers: {
             authtoken,
         },
--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import AdminNav from '../../components/nav/AdminNav';
 
 const AdminDashboard = () => {
     return (
-        <>
-        <p>Admin dashboard</p>
-        </>
-    )
+        <div className="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <AdminNav />
+            </div>
+            <div class="col">admin dashboard page</div>
+        </div>
+    </div>
+)
+    
 }
 
 export default AdminDashboard;
