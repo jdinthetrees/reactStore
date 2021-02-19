fd39bdfb43b2a5810bc9a23f84e0ed26f37b1896
John Dang <jollyjohndang@gmail.com>
2021-02-18 17:26:16 -0800

    single product component

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,8 @@ import SubCreate from './pages/admin/sub/SubCreate';
 import SubUpdate from './pages/admin/sub/SubUpdate';
 import ProductCreate from './pages/admin/product/ProductCreate';
 import AllProducts from './pages/admin/product/AllProducts';
-import ProductUpdate from './pages/admin/product/ProductUpdate'
+import ProductUpdate from './pages/admin/product/ProductUpdate';
+import Product from './pages/Product'
 
 import {auth} from './firebase';
 import {useDispatch} from 'react-redux';
@@ -86,6 +87,7 @@ useEffect(() => {
       <AdminRoute exact path="/admin/product" component ={ProductCreate} />
       <AdminRoute exact path="/admin/products" component ={AllProducts} />
       <AdminRoute exact path="/admin/product/:slug" component ={ProductUpdate} />
+      <Route exact path="/product/:slug" component ={Product} />
      </Switch>
    </>
   );
