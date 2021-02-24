93fbc51a90376297e3c8685a5fc249fa16f618a3
John Dang <jollyjohndang@gmail.com>
2021-02-23 23:23:40 -0800

    redirecting to intended page, api request with rating

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,28 +1,47 @@
-import React, {useEffect, useState} from "react";
-import {getProduct} from "../functions/product";
-import SingleProduct from "../components/cards/SingleProduct"
+import React, { useEffect, useState } from "react";
+import { getProduct, productStar } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
+import {useSelector} from "react-redux";
 
+const Product = ({ match }) => {
+  const [product, setProduct] = useState({});
+  const [star, setStar] = useState(0);
+  //redux
+  const {user} = useSelector((state) => ({...state}));
 
-const Product = ({match}) => {
-    const [product, setProduct] = useState({})
+  const { slug } = match.params;
 
-    const {slug} = match.params;
+  useEffect(() => {
+    loadSingleProduct();
+  }, [slug]);
 
-    useEffect(() => {
-        loadSingleProduct();
-    }, [slug])
+  const loadSingleProduct = () =>
+    getProduct(slug).then((res) => setProduct(res.data));
 
-    const loadSingleProduct = () => 
-        getProduct(slug).then((res) => setProduct(res.data));
+  const onStarClick = (newRating, name) => {
+    setStar(newRating);
+    // console.table(newRating, name);
+    productStar(name, star, user.token)
+    .then(res => {
+        console.log('rating clicked', res.data);
+        loadSingleProduct();//if you want to show updated rating in real time
+    })
+  };
 
-        return <div className="containter-fluid">
-            <div className="row pt-4">
-                <SingleProduct product ={product} />
-            </div>
-            <div className="row">
-                <div>Related Products</div>
-            </div>
-        </div>
+  return (
+    <div className="containter-fluid">
+      <div className="row pt-4">
+        <SingleProduct
+          product={product}
+          onStarClick={onStarClick}
+          star={star}
+        />
+      </div>
+      <div className="row">
+        <div>Related Products</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const User = require("../models/user");
 const slugify = require("slugify");
 
 exports.create = async (req, res) => {
@@ -121,33 +122,69 @@ exports.productsCount = async (req, res) => {
   res.json(total);
 };
 
+// exports.productStar = async (req, res) => {
+//   const product = await Product.findById(req.params.productID).exec();
+//   const user = await User.findOne({ email: req.user.email }).exec();
+//   const { star } = req.body;
+//   //who is updating
+//   //check if currently logged in user has already added rating to product?
+//   let existingRatingObject = product.ratings.find(
+//     (element) => element.postedBy.toString() === user._id.toString()
+//   );
+
+//   //if user hasnt left rating, push rating to ratings array
+//   if (existingRatingObject === undefined) {
+//     let ratingAdded = await Product.findByIdAndUpdate(
+//       product._id,
+//       {
+//         $push: { ratings: { star: star, postedBy: user._id } },
+//       },
+//       { new: true }
+//     ).exec();
+//     console.log("ratingAdded", ratingAdded);
+//     res.json(ratingAdded);
+//   } else {
+//     // user already left rating, update it
+//     const ratingUpdated = await Product.updateOne({
+//       ratings: { $elementMatch: existingRatingObject },
+//     }, {$set: {"ratings.$.star": star}},
+//     { new: true }
+//     ).exec();
+//     console.log("ratingUpdated", ratingUpdated);
+//     res.json(ratingUpdated);
+//   }
+// };
+
 exports.productStar = async (req, res) => {
-  const product = await Product.findById(req.params.productID).exec();
+  const product = await Product.findById(req.params.productId).exec();
   const user = await User.findOne({ email: req.user.email }).exec();
   const { star } = req.body;
-  //who is updating
-  //check if currently logged in user has already added rating to product?
+
+  // who is updating?
+  // check if currently logged in user have already added rating to this product?
   let existingRatingObject = product.ratings.find(
-    (element) => element.postedBy == user._id
+    (ele) => ele.postedBy.toString() === user._id.toString()
   );
 
-  //if user hasnt left rating, push rating to ratings array
+  // if user haven't left rating yet, push it
   if (existingRatingObject === undefined) {
     let ratingAdded = await Product.findByIdAndUpdate(
       product._id,
       {
-        $push: { ratings: { star: star, postedBy: user._id } },
+        $push: { ratings: { star, postedBy: user._id } },
       },
       { new: true }
     ).exec();
     console.log("ratingAdded", ratingAdded);
     res.json(ratingAdded);
   } else {
-    // user already left rating, update it
-    const ratingUpdated = await Product.updateOne({
-      ratings: { $elementMatch: existingRatingObject },
-    }, {$set: {"ratings.$.star": star}},
-    { new: true }
+    // if user have already left rating, update it
+    const ratingUpdated = await Product.updateOne(
+      {
+        ratings: { $elemMatch: existingRatingObject },
+      },
+      { $set: { "ratings.$.star": star } },
+      { new: true }
     ).exec();
     console.log("ratingUpdated", ratingUpdated);
     res.json(ratingUpdated);
